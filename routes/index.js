f3c9fcf681396f36d1cbc3e4c357644acb4450bb
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-30 13:11:41 +0700

    Setting up sequelize connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+//Including dependency
+var Sequelize = require("sequelize");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//Setting up the config
+var sequelize = new Sequelize('personal_information', 'root', 'supermanvsthor', {
+  host: "127.0.0.1",
+  port: 3306,
+  dialect: 'mysql'
 });
+
+//Checking connection status
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = router;
